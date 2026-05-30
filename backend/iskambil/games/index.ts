ac87ba14188b601ed4be4aeb1ec880675d31d217
selimdilsadercan <selimdilsadercan@gmail.com>
2026-05-30 14:34:56 +0300

    update

--- backend/iskambil/games/index.ts
+++ backend/iskambil/games/index.ts
@@ -2,7 +2,7 @@ import game21 from "./casino-betting/21.json";
 import poker from "./casino-betting/poker.json";
 import game31 from "./casino-betting/31.json";
 import blackjack from "./casino-betting/blackjack.json";
-import spiteAndMallice from "./patience-strategy/spite-and-mallice.json";
+import spiteAndMalice from "./patience-strategy/spite-and-malice.json";
 import kosedekiPapazlar from "./patience-strategy/kosedeki-papazlar.json";
 import solitaire from "./patience-strategy/solitaire.json";
 import spiderSolitaire from "./patience-strategy/spider-solitaire.json";
@@ -17,27 +17,27 @@ import speed from "./party-speed/speed.json";
 import peanutsNerts from "./party-speed/peanuts-nerts.json";
 import carouselRummy from "./rummy-meld-building/carousel-rummy.json";
 import game51 from "./rummy-meld-building/51.json";
-import ginRummy2Kisilik101 from "./rummy-meld-building/gin-rummy-2-kisilik-101.json";
-import authorsBana2TanePapazVer from "./matching-capturing/authors-bana-2-tane-papaz-ver.json";
+import ginRummy from "./rummy-meld-building/gin-rummy.json";
+import authors from "./matching-capturing/authors.json";
 import casino from "./matching-capturing/casino.json";
-import dostKazigiKaptiKacti from "./matching-capturing/dost-kazigi-kapti-kacti.json";
+import dostKazigi from "./matching-capturing/dost-kazigi.json";
 import scopa from "./matching-capturing/scopa.json";
-import gopsGameOfPureStrategy from "./matching-capturing/gops-game-of-pure-strategy.json";
+import gops from "./matching-capturing/gops.json";
 import pisti from "./matching-capturing/pisti.json";
 import esliBatak from "./trick-taking-trump/esli-batak.json";
-import game66Santase from "./trick-taking-trump/66-santase.json";
+import sixtySix from "./trick-taking-trump/sixty-six.json";
 import minnesotaWhist from "./trick-taking-trump/minnesota-whist.json";
 import crash from "./trick-taking-trump/crash.json";
 import gommeliBatak from "./trick-taking-trump/gommeli-batak.json";
 import batakSpades from "./trick-taking-trump/batak-spades.json";
-import whistIhalesizIkiKisilikBatak from "./trick-taking-trump/whist-ihalesiz-iki-kisilik-batak.json";
-import ikiliBatakRusBatagi from "./trick-taking-trump/ikili-batak-rus-batagi.json";
+import germanWhist from "./trick-taking-trump/german-whist.json";
+import ikiliBatak from "./trick-taking-trump/ikili-batak.json";
 import cucumber from "./trick-taking-trump/cucumber.json";
 import pinochle from "./trick-taking-trump/pinochle.json";
 import pisYedili from "./shedding/pis-yedili.json";
-import crazyEightsUno from "./shedding/crazy-eights-uno.json";
+import crazyEights from "./shedding/crazy-eights.json";
 import crazyEightsCountdown from "./shedding/crazy-eights-countdown.json";
-import baskanGot from "./shedding/baskan-got.json";
+import baskan from "./shedding/baskan.json";
 import pyramidSolitaire from "./patience-strategy/pyramid-solitaire.json";
 
 export const gamesData = [
@@ -45,7 +45,7 @@ export const gamesData = [
   poker,
   game31,
   blackjack,
-  spiteAndMallice,
+  spiteAndMalice,
   kosedekiPapazlar,
   solitaire,
   spiderSolitaire,
@@ -61,25 +61,25 @@ export const gamesData = [
   peanutsNerts,
   carouselRummy,
   game51,
-  ginRummy2Kisilik101,
-  authorsBana2TanePapazVer,
+  ginRummy,
+  authors,
   casino,
-  dostKazigiKaptiKacti,
+  dostKazigi,
   scopa,
-  gopsGameOfPureStrategy,
+  gops,
   pisti,
   esliBatak,
-  game66Santase,
+  sixtySix,
   minnesotaWhist,
   crash,
   gommeliBatak,
   batakSpades,
-  whistIhalesizIkiKisilikBatak,
-  ikiliBatakRusBatagi,
+  germanWhist,
+  ikiliBatak,
   cucumber,
   pinochle,
   pisYedili,
-  crazyEightsUno,
+  crazyEights,
   crazyEightsCountdown,
-  baskanGot
+  baskan
 ];
