adfca628a72055ff7a56930d9512e9d2845b5697
selimdilsadercan <selimdilsadercan@gmail.com>
2026-06-21 23:00:44 +0300

    fix

--- frontend/lib/config.ts
+++ frontend/lib/config.ts
@@ -1,6 +1,6 @@
 export const APP_CONFIG = {
-  version: "1.1.5",
-  buildNumber: 13,
+  version: "1.1.6",
+  buildNumber: 14,
   name: "Everything",
   supportEmail: "taillesscatstudios@gmail.com",
 } as const;
